a2b37d2d2aa3888ef9c2a9ff5aebd501771ad963
Seyithan Arg <seyithan@MacBook-Pro.local>
2023-01-16 01:31:00 +0000

    third comit

--- classes.py
+++ classes.py
@@ -1,3 +1,24 @@
 class Flight():
     def __init__(self, capacity):
-        
+        self.capacity = capacity
+        self.passengers = []
+
+    def add_passenger(self, name):
+        if not self.open_seats(): # if self.open_seats() == 0:
+            return False
+        self.passengers.append(name)
+        return True
+
+    def open_seats(self):
+        return self.capacity - len(self.passengers)
+
+flight = Flight(3)
+
+people = ["Harry", "Ron", "Hermione", "Ginny"]
+for person in people:
+    success = flight.add_passenger(person)
+    if success:
+        print(f"{person} added to the flight succesfully")
+    else:
+        print(f"The flight is at full capacity! - We are not able to add {person} to this flight.") 
+
