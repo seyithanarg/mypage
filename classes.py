6f6696722bc1ce556cf9654f875e0bc09df26c62
Seyithan Arg <seyithan@MacBook-Pro.local>
2023-01-16 01:14:54 +0000

    my second commit

--- classes.py
+++ classes.py
@@ -1,8 +1,3 @@
-class Point():
-    def __init__(self, input1, input2):
-        self.x = input1
-        self.y = input2
-
-p = Point(2, 8)
-print(p.x)
-print(p.y)
+class Flight():
+    def __init__(self, capacity):
+        
